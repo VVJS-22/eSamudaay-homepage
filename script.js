82624c2bdc3803d7fdad97f58fd7bd4735afe778
Jayesh Sugumaran <s.jayeshchennai@gmail.com>
2021-05-12 08:33:52 +0530

    fifth commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const activeToggle = (element) => {
- deactiveCurrent = document.querySelector(".active");
+ let deactiveCurrent = document.querySelector(".active");
  deactiveCurrent.classList.remove("active");
  element.classList.add("active");
 }
@@ -8,28 +8,29 @@ let search = false;
 
 const setBorder = () => {
  if (search  === false) {
-  element = document.querySelector(".search-bar");
+  let element = document.querySelector(".search-bar");
   element.classList.add("selected");
 
-  searchIcon = document.querySelector(".fas.fa-search");
-  backIcon  = document.createElement("i");
+  let searchIcon = document.querySelector(".fas.fa-search");
+  let backIcon  = document.createElement("i");
   backIcon.setAttribute("class","fas fa-chevron-left");
-  searchIconDiv = document.querySelector(".search-icon");
+  let searchIconDiv = document.querySelector(".search-icon");
   searchIconDiv.removeChild(searchIcon);
   searchIconDiv.appendChild(backIcon);
+  
   search = true;
  }
 }
 
 const removeBorder = (event) => {
  if (search === true) {
- element = document.querySelector(".search-bar");
+ let element = document.querySelector(".search-bar");
  element.classList.remove("selected");
 
- backIcon = document.querySelector(".fas.fa-chevron-left");
- searchIcon  = document.createElement("i");
+ let backIcon = document.querySelector(".fas.fa-chevron-left");
+ let searchIcon  = document.createElement("i");
  searchIcon.setAttribute("class","fas fa-search");
- searchIconDiv = document.querySelector(".search-icon");
+ let searchIconDiv = document.querySelector(".search-icon");
  searchIconDiv.removeChild(backIcon);
  searchIconDiv.appendChild(searchIcon);
  event.stopPropagation();
@@ -37,8 +38,52 @@ const removeBorder = (event) => {
  }
 }
 
-searchBar = document.querySelector("#search");
+let searchBar = document.querySelector("#search");
 searchBar.addEventListener('click', setBorder);
 
-container = document.querySelector(".search-icon");
+let container = document.querySelector(".search-icon");
 container.addEventListener('click', removeBorder);
+
+let box = document.querySelectorAll(".video-container");
+let width = box[0].offsetWidth + 30;
+let slide = document.querySelector(".slide");
+slide.style.minWidth = `${box.length * width}px`;
+let start;
+let change;
+
+let videosContainer = document.querySelector(".product-videos");
+videosContainer.addEventListener('dragstart', (e) => {
+ start = e.clientX;
+})
+
+videosContainer.addEventListener('dragover', (e) => {
+ e.preventDefault();
+ let touch = e.clientX;
+ change = start - touch;
+})
+
+let slideshow = () => {
+ if (change > 0) {
+  videosContainer.scrollLeft += window;
+ }else {
+  videosContainer.scrollLeft += window;
+ }
+
+}
+
+videosContainer.addEventListener('dragend', slideshow);
+
+
+videosContainer.addEventListener('touchstart', (e) => {
+ start = e.touches[0].clientX;
+})
+
+videosContainer.addEventListener('touchover', (e) => {
+ e.preventDefault();
+ let touch = e.touches[0].clientX;
+ change = start - touch;
+})
+
+
+videosContainer.addEventListener('touchend', slideshow);
+
